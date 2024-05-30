279c7910042a96c743e36e46f03a970151efa169
exocornet <exocornet@gmail.com>
2024-05-30 14:24:45 +0300

    feat: refactoring
    refactoring

---
+++ configurations/images-conversion-webp.js
@@ -0,0 +1,20 @@
+// import('imagemin').then((imagemin) => {
+// 	import('imagemin-webp').then((imageminWebp) => {
+// 		// Оптимизация и конвертация изображений
+// 		imagemin
+// 			.default(['test/images/**/*.{jpg,png}'], {
+// 				destination: 'dist/images',
+// 				plugins: [
+// 					imageminWebp.default({
+// 						quality: 100,
+// 					}),
+// 				],
+// 			})
+// 			.then(() => {
+// 				console.log('Images have been processed successfully.');
+// 			})
+// 			.catch((error) => {
+// 				console.error('An error occurred during image processing:', error);
+// 			});
+// 	});
+// }),
